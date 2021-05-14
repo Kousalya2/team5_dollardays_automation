bef8833a395f2a8db2215aeced0264c705e6e1b5
Kousalya2 <84060792+Kousalya2@users.noreply.github.com>
2021-05-14 11:47:17 -0700

    Initial commit of DollarDays
    This contains place holder for maven project, POM file and selenium, testNG dependencies.

---
+++ src/test/java/sanity/testcases/DollarDaysFooter.java
@@ -0,0 +1,12 @@
+package sanity.testcases;
+
+import org.testng.annotations.Test;
+
+public class DollarDaysFooter {
+	
+	@Test
+	private void validateFooterLinks() {
+		System.out.println("TESTING");
+	}
+
+}
